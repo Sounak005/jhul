8d873ffccce9b1121be7edc28bbdde124e8c6c7e
Sounak Gupta <sounakgupta8285@gmail.com>
2026-03-12 01:08:58 +0530

    justlikethat
    just

---
+++ io.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset("tips")
+
+sns.scatterplot(
+    x="total_bill",
+    y="tip",
+    hue="sex",        # color
+    size="size",      # size of dots
+    data=df
+)
+
+plt.show()
